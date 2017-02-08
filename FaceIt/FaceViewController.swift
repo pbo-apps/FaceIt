f33d3ae1771bd9a63779e9c36f5dc7e412b4e0ad
Pete Bounford <petebounford@gmail.com>
2017-02-08 13:35:31 +0100

    Add model for a facial expression and get the controller to interpret the model and update the view

--- FaceIt/FaceViewController.swift
+++ FaceIt/FaceViewController.swift
@@ -10,16 +10,27 @@ import UIKit
 
 class FaceViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var expression = FacialExpression(eyes: .Open, eyeBrows: .Normal, mouth: .Smile) {
+        didSet {
+            updateUI()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet weak var faceView: FaceView! { didSet { updateUI() } }
+    
+    private var mouthCurvatures = [FacialExpression.Mouth.Frown:-1.0,.Grin:1.0,.Smile:0.5,.Smirk:0.2,.Neutral:0.0]
+    
+    private var eyeBrowTilts = [FacialExpression.EyeBrows.Relaxed:0.5,.Furrowed:-0.5,.Normal:0.0]
+    
+    private func updateUI() {
+        switch expression.eyes {
+        case .Open: faceView.eyesOpen = true
+        case .Closed: faceView.eyesOpen = false
+        case .Squinting: faceView.eyesOpen = false
+        }
+        faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+        faceView.eyeBrowTilt = eyeBrowTilts[expression.eyeBrows] ?? 0.0
     }
-
-
+    
 }
 
---
+++ FaceIt/FacialExpression.swift
@@ -0,0 +1,49 @@
+//
+//  FacialExpression.swift
+//  FaceIt
+//
+//  Created by Pete Bounford on 08/02/2017.
+//  Copyright © 2017 PBO Apps. All rights reserved.
+//
+
+import Foundation
+
+struct FacialExpression {
+    var eyes: Eyes
+    var eyeBrows: EyeBrows
+    var mouth: Mouth
+    
+    enum Eyes: Int {
+        case Open
+        case Closed
+        case Squinting
+    }
+    
+    enum EyeBrows: Int {
+        case Relaxed
+        case Normal
+        case Furrowed
+        
+        func moreRelaxedBrow() -> EyeBrows {
+            return EyeBrows(rawValue: rawValue - 1) ?? .Relaxed
+        }
+        func moreFurrowedBrow() -> EyeBrows {
+            return EyeBrows(rawValue: rawValue + 1) ?? .Furrowed
+        }
+    }
+    
+    enum Mouth: Int {
+        case Frown
+        case Smirk
+        case Neutral
+        case Grin
+        case Smile
+        
+        func sadderMouth() -> Mouth {
+            return Mouth(rawValue: rawValue - 1) ?? .Frown
+        }
+        func happierMouth() -> Mouth {
+            return Mouth(rawValue: rawValue + 1) ?? .Smile
+        }
+    }
+}
