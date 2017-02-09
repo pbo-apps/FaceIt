043fc826e64f46ef69940763ca3078a65a4db311
Pete Bounford <petebounford@gmail.com>
2017-02-09 16:25:13 +0100

    Implement prepareForSegue to update the UI when selecting different emotions

--- FaceIt/EmotionsViewController.swift
+++ FaceIt/EmotionsViewController.swift
@@ -10,14 +10,23 @@ import UIKit
 
 class EmotionsViewController: UIViewController {
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    private let emotionalFaces: Dictionary<String,FacialExpression> = [
+        "angry": FacialExpression(eyes: .Closed, eyeBrows: .Furrowed, mouth: .Frown),
+        "happy": FacialExpression(eyes: .Open, eyeBrows: .Normal, mouth: .Smile),
+        "worried": FacialExpression(eyes: .Open, eyeBrows: .Relaxed, mouth: .Smirk),
+        "mischievous": FacialExpression(eyes: .Open, eyeBrows: .Furrowed, mouth: .Grin),
+    ]
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let destinationVC = segue.destination
+        if let faceVC = destinationVC as? FaceViewController {
+            if let identifier = segue.identifier {
+                if let expression = emotionalFaces[identifier] {
+                    faceVC.expression = expression
+                }
+            }
+        }
+        
     }
-    */
 
 }
